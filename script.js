f149a4ddfb7a449b6269841bf13a490fd839436d
Priyansh <priyanshrai16@gmail.com>
2023-03-01 01:39:13 +0530

    Modified script.js to handle errors during fetch

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ let adviceID = document.querySelector(".advice-id");
 let adviceSpan = document.querySelector(".advice > span");
 
 let populateAdvice = async () => {
-    let response = await fetch(
-        "https://api.adviceslip.com/advice?t=" + Math.random()
-    );
-    let advice = (await response.json()).slip;
-    adviceSpan.textContent = advice.advice;
-    adviceID.textContent = advice.id;
+    try {
+        let response = await fetch(
+            "https://api.adviceslip.com/advice?t=" + Math.random()
+        );
+        let advice = (await response.json()).slip;
+        adviceSpan.textContent = advice.advice;
+        adviceID.textContent = advice.id;
+    } catch (error) {
+        console.log("An error occurred while fetching advice:", error);
+    }
 };
 
 regenerateButton.addEventListener("click", populateAdvice);
